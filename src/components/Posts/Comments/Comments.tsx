21eb2c8e6c25f5e8e3f9d610e61cc526d69a0c82
See-Its-Manish <seeitsmanish@gmail.com>
2024-09-01 21:22:02 +0530

    fix: application breaking on clicking any post, when user not logged in

--- src/components/Posts/Comments/Comments.tsx
+++ src/components/Posts/Comments/Comments.tsx
@@ -215,7 +215,7 @@ const Comments: React.FC<CommentsProps> = ({
                         comment={comment}
                         onDeleteComment={onDeleteComment}
                         loadingDelete={loadingDeleteId === comment.id}
-                        userId={user.uid}
+                        userId={user?.uid}
                       />
                     )}
                   </>
